3a09f92474f9fb0bd941cece1a1a471dfd7331ac
Its_Shojib <mdshojib922@gmail.com>
2023-12-20 20:27:31 +0600

    Hooks Added

--- src/Hooks/useAuth.jsx
+++ src/Hooks/useAuth.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
-
 const useAuth = () => {
     const auth = useContext(AuthContext);
     return auth;
